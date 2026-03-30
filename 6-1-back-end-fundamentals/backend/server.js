b66cf267c6d81db8c90ec8e4da7ce339ad90e421
kawthar1235 <s202168810@kfupm.edu.sa>
2026-03-30 12:55:24 +0300

    task7

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -25,7 +25,9 @@ app.get("/about", (req, res) => {
 
 // TODO 7: Create the "/student" route
 
-
+app.get("/student", (req, res) => {
+  res.send("Student Name: Kawthar");
+});
 
 // TODO 4: Start the server on port 3000
 
