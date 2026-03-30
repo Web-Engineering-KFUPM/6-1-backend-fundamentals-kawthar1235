26e556b7b74cd215dc4a2a4f72998da5effd8d04
kawthar1235 <s202168810@kfupm.edu.sa>
2026-03-30 12:53:15 +0300

    task2

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 // TODO 2: Create the Express app and store it in a variable named app
 
-
+const app = express();
 
 // TODO 3: Allow React to access the server
 
