6dec61c874692bba317ff49d97f83ffaea2ec66f
kawthar1235 <s202168810@kfupm.edu.sa>
2026-03-30 12:58:27 +0300

    task1

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -1,36 +1,29 @@
 // TODO 1: Import Express
-
-const express = require("express");
+import express from "express";
+import cors from "cors";
 
 // TODO 2: Create the Express app and store it in a variable named app
-
 const app = express();
 
 // TODO 3: Allow React to access the server
-
-const cors = require("cors");
 app.use(cors());
 
 // TODO 5: Create the home route "/"
-
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page");
 });
 
 // TODO 6: Create the "/about" route
-
 app.get("/about", (req, res) => {
   res.send("This is the About Page");
 });
 
 // TODO 7: Create the "/student" route
-
 app.get("/student", (req, res) => {
   res.send("Student Name: Kawthar");
 });
 
 // TODO 4: Start the server on port 3000
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
